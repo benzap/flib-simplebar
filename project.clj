39eaf3049cba313028718c338be259eff210501e
Benjamin Zaporzan <benzaporzan@gmail.com>
2019-11-28 22:50:10 -0500

    First Commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject flib/simplebar "5.0.7-SNAPSHOT"
+  :description "CLJS Foreign Library - simplebar"
+  :url "http://github.com/benzap/flib-simplebar"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"})
---
+++ src/deps.cljs
@@ -0,0 +1,5 @@
+{:foreign-libs
+ [{:file "flib/simplebar/simplebar.js",
+   :file-min "flib/simplebar/simplebar.min.js"
+   :provides ["flib.simplebar"]}],
+ :externs ["flib/simplebar/simplebar.ext.js"]}
