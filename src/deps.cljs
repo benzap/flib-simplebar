7cec2231fa58d5791c5b1fab7c9c782f0d51c280
Benjamin Zaporzan <ben@district0x.io>
2019-12-03 18:23:42 -0500

    More Fixes

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,6 +1,5 @@
 {:foreign-libs
  [{:file "flib/simplebar/simplebar.js",
    :file-min "flib/simplebar/simplebar.min.js"
-   :provides ["flib.simplebar" "simplebar"]
-   :global-exports '{simplebar SimpleBar},}]
+   :provides ["flib.simplebar" "simplebar"]}]
  :externs ["flib/simplebar/simplebar.ext.js"]}
